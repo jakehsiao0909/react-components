0ac635c8af765a1ee8fa3c11f777c6710c9c9262
jakehsiao0909 <jakehsiao0909@gmail.com>
2018-03-21 14:30:54 -0500

    completed

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// TODO
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bought: false
+        };
+    }
+
+    onListItemHover() {
+        this.setState({
+            bought: !this.state.bought
+        });
+    }
+
+    render() {
+        var style = {
+            fontWeight: this.state.bought ? 'bold' : 'normal'
+        };
+
+        return (
+            <li style={style} onMouseOver={() => this.onListItemHover()}> {this.props.groceryItems}</li>
+        )
+    }
+}
+
+var GroceryList = (props) => (
+
+    <ul>
+        {props.groceryItems.map(item => 
+            <GroceryListItem groceryItems ={item} />
+        )}
+    </ul>
+)
+
+ReactDOM.render(<GroceryList groceryItems={['Candy', 'Chocolate', 'Ice Cream']} />, document.getElementById('app'));
